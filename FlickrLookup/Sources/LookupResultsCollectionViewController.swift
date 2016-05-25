e821d692e106986b17b1e891c54e9e4a6c2d7423
alonso <alonskiy@gmail.com>
2016-05-25 20:31:32 +0200

    change cells appearnce
    minor fixes

--- FlickrLookup/Sources/LookupResultsCollectionViewController.swift
+++ FlickrLookup/Sources/LookupResultsCollectionViewController.swift
@@ -11,12 +11,11 @@ import UIKit
 
 class LookupResultsCollectionViewController: UICollectionViewController {
     
-    private static let defaultThumbnailSize = CGSize(width: 100.0, height: 100.0)
-
     private var photos = [Photo]()
     private let flickrLookup = FlickrLookup()
     private let flickrPhotosLoader = FlickrPhotosLoader()
     private var fetchingInProgress = false
+    private var cellSizeCache: CGSize?
     
     private let placeholderImage = UIImage(named: "placeholder")!
     
@@ -85,25 +84,38 @@ class LookupResultsCollectionViewController: UICollectionViewController {
         return cell
     }
     
-    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
-        let photo = photos[indexPath.row]
-
-        if let thumbnailSize = photo.thumbnailSize {
-            return thumbnailSize
-        } else if photo.thumbnail != nil {
-            return photo.sizeToAspectFitPhotoIntoSize(self.dynamicType.defaultThumbnailSize)
-        }
-
-        return placeholderImage.size
-    }
-    
     override func scrollViewDidScroll(scrollView: UIScrollView) {
         if !fetchingInProgress && flickrLookup.canRequestNextPage() {
-            let lastScreenYOffset = collectionView!.contentSize.height - collectionView!.bounds.size.height
+            let lastScreenYOffset = collectionView!.contentSize.height - 2.0 * collectionView!.bounds.size.height
             if lastScreenYOffset < collectionView!.contentOffset.y {
                 fetchingInProgress = flickrLookup.next()
             }
         }
+    }
+    
+    
+    override func viewWillTransitionToSize(size: CGSize, withTransitionCoordinator coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransitionToSize(size, withTransitionCoordinator: coordinator)
+        
+        cellSizeCache = nil
+        collectionView?.reloadData()
+    }
+
+    
+    // MARK: UICollectionViewDelegateFlowLayout
+    
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+        if let cellSizeCache = cellSizeCache {
+            return cellSizeCache
+        }
+        
+        let numberOfItemsPerRow = collectionView.bounds.height > collectionView.bounds.width ? 4 : 6
+        
+        let flowLayout = collectionViewLayout as! UICollectionViewFlowLayout
+        let sideSize = floor((collectionView.bounds.width - flowLayout.minimumInteritemSpacing * CGFloat(numberOfItemsPerRow - 1)) / CGFloat(numberOfItemsPerRow))
+
+        cellSizeCache = CGSize(width: sideSize, height: sideSize)
+        return cellSizeCache!
     }
 }
 

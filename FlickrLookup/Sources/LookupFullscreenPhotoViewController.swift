c73480943b360d3778d172b5081cc4af04e04ed5
alonso <alonskiy@gmail.com>
2016-06-05 22:31:02 +0200

    photo zooming

--- FlickrLookup/Sources/LookupFullscreenPhotoViewController.swift
+++ FlickrLookup/Sources/LookupFullscreenPhotoViewController.swift
@@ -12,6 +12,13 @@ class LookupFullscreenPhotoViewController: UIViewController {
 
     @IBOutlet private weak var photoImageView: UIImageView!
     
+    @IBOutlet private weak var scrollView: UIScrollView!
+    @IBOutlet private weak var imageViewBottomConstraint: NSLayoutConstraint!
+    @IBOutlet private weak var imageViewLeadingConstraint: NSLayoutConstraint!
+    @IBOutlet private weak var imageViewTopConstraint: NSLayoutConstraint!
+    @IBOutlet private weak var imageViewTrailingConstraint: NSLayoutConstraint!
+    @IBOutlet private weak var activityIndicator: UIActivityIndicatorView!
+    
     var photo: Photo?
     var flickrPhotosLoader: FlickrPhotosLoader?
     
@@ -31,14 +38,68 @@ class LookupFullscreenPhotoViewController: UIViewController {
         guard let photo = photo else { return }
         if photo.photo != nil {
             photoImageView.image = photo.photo
+            photoImageView.hidden = false
+            photoImageView.sizeToFit()
         } else {
             UIApplication.sharedApplication().networkActivityIndicatorVisible = true
+            activityIndicator.hidden = false
+            activityIndicator.startAnimating()
             flickrPhotosLoader?.load(photo) { [weak self] successfully in
+                self?.activityIndicator.stopAnimating()
                 if successfully {
                     self?.photoImageView.image = photo.photo
+                    self?.photoImageView.hidden = false
+                    self?.photoImageView.sizeToFit()
+                } else {
+                    //TODO: show message
                 }
                 UIApplication.sharedApplication().networkActivityIndicatorVisible = false
             }
         }
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        updateMinZoomScaleForSize(view.bounds.size)
+    }
+    
+    
+    //MARK: - Zooming
+    
+    private func updateMinZoomScaleForSize(size: CGSize) {
+        let widthScale = size.width / photoImageView.bounds.width
+        let heightScale = size.height / photoImageView.bounds.height
+        let minScale = min(widthScale, heightScale)
+        
+        scrollView.minimumZoomScale = minScale
+        
+        scrollView.zoomScale = minScale
+    }
+    
+    private func updateConstraintsForSize(size: CGSize) {
+        
+        let yOffset = max(0, round(size.height - photoImageView.frame.height) / 2)
+        imageViewTopConstraint.constant = yOffset
+        imageViewBottomConstraint.constant = yOffset
+        
+        let xOffset = max(0, round(size.width - photoImageView.frame.width) / 2)
+        imageViewLeadingConstraint.constant = xOffset
+        imageViewTrailingConstraint.constant = xOffset
+        
+        view.layoutIfNeeded()
+    }
+}
+
+
+//MARK: - UIScrollViewDelegate
+
+extension LookupFullscreenPhotoViewController: UIScrollViewDelegate {
+    func viewForZoomingInScrollView(scrollView: UIScrollView) -> UIView? {
+        return photoImageView
+    }
+    
+    func scrollViewDidZoom(scrollView: UIScrollView) {
+        updateConstraintsForSize(view.bounds.size)
     }
 }

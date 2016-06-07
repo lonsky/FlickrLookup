06ad2573520e5574c25cbe481586a13b6ce16976
alonso <alonskiy@gmail.com>
2016-06-07 22:52:02 +0200

    minor fix

--- FlickrLookup/Sources/FlickrURLFactory.swift
+++ FlickrLookup/Sources/FlickrURLFactory.swift
@@ -9,20 +9,20 @@
 import Foundation
 
 
-class FlickrURLFactory {
+struct FlickrURLFactory {
     private static let apiKey = "babe050614b0895f347495e4e79b606e"
     
-    class func lookupURL(text: String, page: Int = 1, itemsPerPage: Int = 100) -> NSURL? {
+    static func lookupURL(text: String, page: Int = 1, itemsPerPage: Int = 100) -> NSURL? {
         guard let escapedText = text.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLQueryAllowedCharacterSet()) else { return nil }
         
         return NSURL(string: "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=\(apiKey)&text=\(escapedText)&page=\(page)&per_page=\(itemsPerPage)&format=json&nojsoncallback=1")
     }
 
-    class func photoURL(photo: Photo, size: String = "m") -> NSURL? {
+    static func photoURL(photo: Photo, size: String = "m") -> NSURL? {
         return NSURL(string: "https://farm\(photo.farm).staticflickr.com/\(photo.server)/\(photo.id)_\(photo.secret)_\(size).jpg")
     }
     
-    class func photoInfoURL(photo: Photo) -> NSURL? {
+    static func photoInfoURL(photo: Photo) -> NSURL? {
         return NSURL(string: "https://api.flickr.com/services/rest/?method=flickr.photos.getInfo&api_key=\(apiKey)&photo_id=\(photo.id)&secret=\(photo.secret)&format=json&nojsoncallback=1")
     }
 }
